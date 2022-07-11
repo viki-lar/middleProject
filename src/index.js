5eeb18e535c4b4fa89ac25c8d3feea4388a1d16c
«viktoriya <viki.lar@yandex.ru>
2022-07-11 20:34:28 +0400

    таймер и модальное окно реализовано на всех страницах

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import modal from "./modules/modal";
 import timer from "./modules/timer";
+import calc from "./modules/calc";
 
 modal();
 timer("15 july 2022");
+calc();
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,32 +1,33 @@
 import { animate } from "./helpers";
 
 const modal = () => {
-  const headerBtn = document.getElementById("header-btn");
+  const headerBtn = document.querySelectorAll(".header-btn");
   const modal = document.querySelector(".header-modal");
   const overlay = document.querySelector(".overlay");
 
   //вызов модульного окна при нажатии на кнопку
-
-  headerBtn.addEventListener("click", () => {
-    // проверка экрана устройства
-    if (screen.width <= 768) {
-      modal.style.display = "block";
-      overlay.style.display = "block";
-    } else {
-      // анимация
-      modal.style.display = "block";
-      overlay.style.display = "block";
-      animate({
-        duration: 1000,
-        timing(timeFraction) {
-          return Math.pow(timeFraction, 2);
-        },
-        draw(progress) {
-          const windowInnerHeight = document.documentElement.clientHeight;
-          modal.style.top = progress * (windowInnerHeight / 3 + 200) + "px";
-        },
-      });
-    }
+  headerBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      // проверка экрана устройства
+      if (screen.width <= 768) {
+        modal.style.display = "block";
+        overlay.style.display = "block";
+      } else {
+        // анимация
+        modal.style.display = "block";
+        overlay.style.display = "block";
+        animate({
+          duration: 1000,
+          timing(timeFraction) {
+            return Math.pow(timeFraction, 2);
+          },
+          draw(progress) {
+            const windowInnerHeight = document.documentElement.clientHeight;
+            modal.style.top = progress * (windowInnerHeight / 3 + 200) + "px";
+          },
+        });
+      }
+    });
   });
 
   //закрытие модального окна по клику мимо и при нажатии на кнопку закрыть
