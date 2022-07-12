5b29200c6041a0c0fb635623fd9ee94d671fa884
«viktoriya <viki.lar@yandex.ru>
2022-07-12 22:28:33 +0400

    исправлены ошибки в калькуляторе

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -3,64 +3,68 @@
 import { animate } from "./helpers";
 
 const calc = (price) => {
-  const calcBlock = document.querySelector("#calc");
-  const calcType = document.querySelector("#calc-type");
-  const calcTypeMaterial = document.querySelector("#calc-type-material");
-  const calcSquare = document.querySelector("#calc-input");
-  const total = document.querySelector("#calc-total");
+  try {
+    const calcBlock = document.querySelector("#calc");
+    const calcType = document.querySelector("#calc-type");
+    const calcTypeMaterial = document.querySelector("#calc-type-material");
+    const calcSquare = document.querySelector("#calc-input");
+    const total = document.querySelector("#calc-total");
 
-  const countCalc = () => {
-    const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
-    let calcSquareValue = 1;
-    let totalValue = 0;
-    let calcTypeMaterialValue;
+    calcSquare.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/\D+/, "");
+    });
 
-    //определение значений по умолчанию
-    if (calcTypeMaterial.value > 0) {
-      calcTypeMaterialValue =
-        +calcTypeMaterial.options[calcTypeMaterial.selectedIndex].value;
-    } else {
-      calcTypeMaterialValue = 1;
-    }
+    const countCalc = () => {
+      const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
+      let calcSquareValue = calcSquare.value;
+      let totalValue = 0;
+      let calcTypeMaterialValue;
 
-    calcSquareValue = calcSquare.value;
+      //определение значений по умолчанию
+      if (calcTypeMaterial.value > 0) {
+        calcTypeMaterialValue =
+          +calcTypeMaterial.options[calcTypeMaterial.selectedIndex].value;
+      } else {
+        calcTypeMaterialValue = 1;
+      }
 
-    // рассчет при обязательных полях
-    if (calcTypeValue && calcSquareValue) {
-      totalValue =
-        price * calcSquareValue * calcTypeValue * calcTypeMaterialValue;
+      // рассчет при обязательных полях
+      if (calcTypeValue && calcSquareValue) {
+        totalValue =
+          price * calcSquareValue * calcTypeValue * calcTypeMaterialValue;
 
-      // анимация
+        // анимация
 
-      animate({
-        duration: 500,
-        timing(timeFraction) {
-          return timeFraction;
-        },
-        draw(progress) {
-          total.value = Math.trunc(
-            (totalValue * Math.trunc(progress * 10)) / 10
-          );
-        },
-      });
-    } else {
-      totalValue = 0;
-    }
+        animate({
+          duration: 500,
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          draw(progress) {
+            total.value = Math.trunc(
+              (totalValue * Math.trunc(progress * 10)) / 10
+            );
+          },
+        });
+      } else {
+        totalValue = 0;
+      }
 
-    //вывод итога
-    total.value = totalValue;
-  };
+      //вывод итога
+      total.value = totalValue;
+    };
 
-  //запуск калькулятора
-  calcBlock.addEventListener("input", (e) => {
-    if (
-      e.target === calcType ||
-      e.target === calcSquare ||
-      e.target === calcTypeMaterial
-    ) {
-      countCalc();
-    }
-  });
+    //запуск калькулятора
+    calcBlock.addEventListener("input", (e) => {
+      if (
+        e.target === calcType ||
+        e.target === calcSquare ||
+        e.target === calcTypeMaterial
+      ) {
+        countCalc();
+      }
+    });
+  } catch (error) {}
 };
 
 export default calc;
--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,113 +1,115 @@
 "use strict";
 
 const slider = () => {
-  const sliderBlock = document.querySelector(".slider");
-  const slides = document.querySelectorAll(".service-block");
+  try {
+    const sliderBlock = document.querySelector(".slider");
+    const slides = document.querySelectorAll(".service-block");
 
-  const timeInterval = 3000;
-  let currentSlide = 0;
-  let count;
-  let interval;
+    const timeInterval = 3000;
+    let currentSlide = 0;
+    let count;
+    let interval;
 
-  //переключение на следующий слайд
-  const prevSlide = (elems, index) => {
-    if (screen.width >= 576) {
-      elems[index].style.display = "none";
-      elems[index + 1].style.display = "none";
-    } else {
-      elems[index].style.display = "none";
-    }
-  };
-  //переключение на предыдущий слайд
-  const nextSlide = (elems, index) => {
-    if (screen.width >= 576) {
-      elems[index].style.display = "block";
-      elems[index + 1].style.display = "block";
-    } else {
-      elems[index].style.display = "block";
-    }
-  };
+    //переключение на следующий слайд
+    const prevSlide = (elems, index) => {
+      if (screen.width >= 576) {
+        elems[index].style.display = "none";
+        elems[index + 1].style.display = "none";
+      } else {
+        elems[index].style.display = "none";
+      }
+    };
+    //переключение на предыдущий слайд
+    const nextSlide = (elems, index) => {
+      if (screen.width >= 576) {
+        elems[index].style.display = "block";
+        elems[index + 1].style.display = "block";
+      } else {
+        elems[index].style.display = "block";
+      }
+    };
 
-  // автоматическое переключение слайдов
-  const autoSlide = () => {
-    prevSlide(slides, currentSlide);
-    if (screen.width >= 576) {
-      currentSlide += +2;
-    } else {
-      currentSlide++;
-    }
-    if (currentSlide >= slides.length) {
-      currentSlide = 0;
-    }
-    nextSlide(slides, currentSlide);
-  };
-
-  //запуск автоматического переключения
-  const startSlide = (timer = 2000) => {
-    interval = setInterval(autoSlide, timer);
-  };
-  const stopSlide = () => {
-    clearInterval(interval);
-  };
-
-  //переключение по кнопкам
-  sliderBlock.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    if (e.target.classList.contains("services__arrow")) {
-      return;
-    }
-    prevSlide(slides, currentSlide);
-
-    if (e.target.closest(".services__arrow--right")) {
+    // автоматическое переключение слайдов
+    const autoSlide = () => {
+      prevSlide(slides, currentSlide);
       if (screen.width >= 576) {
         currentSlide += +2;
       } else {
         currentSlide++;
       }
-    } else if (e.target.closest(".services__arrow--left")) {
-      if (screen.width >= 576) {
-        currentSlide -= -2;
-      } else {
-        currentSlide--;
+      if (currentSlide >= slides.length) {
+        currentSlide = 0;
       }
-    }
+      nextSlide(slides, currentSlide);
+    };
 
-    //проверка счетчика и длины массива со слайдами
-    if (currentSlide >= slides.length) {
-      currentSlide = 0;
-    }
-    if (currentSlide < 0) {
-      currentSlide = slides.length - 1;
-    }
+    //запуск автоматического переключения
+    const startSlide = (timer = 2000) => {
+      interval = setInterval(autoSlide, timer);
+    };
+    const stopSlide = () => {
+      clearInterval(interval);
+    };
 
-    nextSlide(slides, currentSlide);
-  });
+    //переключение по кнопкам
+    sliderBlock.addEventListener("click", (e) => {
+      e.preventDefault();
 
-  //остановка слайдера при наведении на кнопки
-  sliderBlock.addEventListener(
-    "mouseenter",
-    (e) => {
-      if (e.target.matches(".services__arrow")) {
-        stopSlide();
+      if (e.target.classList.contains("services__arrow")) {
+        return;
       }
-    },
-    true
-  );
+      prevSlide(slides, currentSlide);
 
-  //запуск слайдера после наведения на кнопки
-
-  sliderBlock.addEventListener(
-    "mouseleave",
-    (e) => {
-      if (e.target.matches(".services__arrow")) {
-        startSlide(timeInterval);
+      if (e.target.closest(".services__arrow--right")) {
+        if (screen.width >= 576) {
+          currentSlide += +2;
+        } else {
+          currentSlide++;
+        }
+      } else if (e.target.closest(".services__arrow--left")) {
+        if (screen.width >= 576) {
+          currentSlide -= -2;
+        } else {
+          currentSlide--;
+        }
       }
-    },
-    true
-  );
 
-  startSlide(timeInterval);
+      //проверка счетчика и длины массива со слайдами
+      if (currentSlide >= slides.length) {
+        currentSlide = 0;
+      }
+      if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+      }
+
+      nextSlide(slides, currentSlide);
+    });
+
+    //остановка слайдера при наведении на кнопки
+    sliderBlock.addEventListener(
+      "mouseenter",
+      (e) => {
+        if (e.target.matches(".services__arrow")) {
+          stopSlide();
+        }
+      },
+      true
+    );
+
+    //запуск слайдера после наведения на кнопки
+
+    sliderBlock.addEventListener(
+      "mouseleave",
+      (e) => {
+        if (e.target.matches(".services__arrow")) {
+          startSlide(timeInterval);
+        }
+      },
+      true
+    );
+
+    startSlide(timeInterval);
+  } catch (error) {}
 };
 
 export default slider;
